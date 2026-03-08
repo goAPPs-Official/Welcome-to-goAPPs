fc91b5f4deb21d51ff6cb11e8bb3378ea834a409
goAPPs Official <fromgoapps@gmail.com>
2026-03-08 17:53:48 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'dsa-cache-v1';
+const assets = [
+  '/',
+  'index.html',
+  'DSA Logo White.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
